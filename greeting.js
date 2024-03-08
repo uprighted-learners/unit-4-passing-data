aec8161b995a2377cf0fd83b556fb6b778b04784
Ben Anderson <andersjbe@gmail.com>
2024-03-08 11:07:03 -0500

    Completed solution

--- greeting.js
+++ greeting.js
@@ -1,1 +1,7 @@
-// Your code goes here
+const friendGreeting = document.querySelector("#friend-greeting");
+
+// Getting the name from the URL
+const params = new URLSearchParams(document.location.search);
+const friendName = params.get("name");
+
+friendGreeting.textContent = `Welcome ${friendName}!`;
